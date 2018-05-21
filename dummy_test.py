0c77edb22fb871bcf3f56e023ad6183844ff3576
Eloy Coto <eloy.coto@gmail.com>
2018-05-21 08:35:04 +0200

    Create some dummy files
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- dummy_test.py
+++ dummy_test.py
@@ -8,13 +8,19 @@ def test_b():
 
 def test_c():
     print "[[ATTACHMENT|/absolute/path/to/some/file]]"
-    assert "aa" == "aa123"
+    assert "aa" == "aa"
 
 def test_d():
+    f = open("test_d.txt","w+")
+    f.write("My new test")
+    f.close()
     print "[[ATTACHMENT|/absolute/path/to/some/file]]"
     assert "aa" == "aa"
 
 def test_j():
+    f = open("test_j.txt","w+")
+    f.write("My new test")
+    f.close()
     assert "aa" == "aa"
 
 @unittest.skip("temporarily disabled")
